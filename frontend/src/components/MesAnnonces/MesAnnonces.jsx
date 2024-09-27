86aed81750e96d2325620643529186a5ca2ecc7e
Ines Saidi <2163109@cmontmorency.qc.ca>
2024-09-27 08:26:11 -0400

    push tous

--- frontend/src/components/MesAnnonces/MesAnnonces.jsx
+++ frontend/src/components/MesAnnonces/MesAnnonces.jsx
@@ -18,33 +18,30 @@ const MesAnnonces = ({ connectedEmployerEmail }) => {
 
   const filteredEmploiList = emplois.filter(emploi => emploi.email_employeur === connectedEmployerEmail);
 
+  // Ajoute un console.log pour déboguer
+  console.log("Annonces filtrées : ", filteredEmploiList);
+  
   return (
-<div> 
-<ul className="lmj-emploi-list">
-
+    <div> 
+      <ul className="lmj-emploi-list">
         {filteredEmploiList.length > 0 ? (
-
           filteredEmploiList.map((emploi) => (
-<div className="emploi-container" key={emploi.nom_poste}>
-<h3 className="jobTitle">{emploi.nom_poste}</h3>
-<span className="jobEntreprise">{emploi.nom_entreprise}</span>
-<span className="jobSector">{emploi.categorie}</span>
-<span className="jobSalary">{emploi.salaire}</span>
-<span className="jobLocation">{emploi.emplacement}</span>
-<div className="button-container">
-<button className="buttonx" onClick={() => handleDelete(emploi)}>Supprimer</button>
-</div>
-</div>
-
+            <div className="emploi-container" key={emploi.nom_poste}>
+              <h3 className="jobTitle">{emploi.nom_poste}</h3>
+              <span className="jobEntreprise">{emploi.nom_entreprise}</span>
+              <span className="jobSector">{emploi.categorie}</span>
+              <span className="jobSalary">{emploi.salaire}</span>
+              <span className="jobLocation">{emploi.emplacement}</span>
+              <div className="button-container">
+                <button className="buttonx" onClick={() => handleDelete(emploi)}>Supprimer</button>
+              </div>
+            </div>
           ))
-
         ) : (
-<li>Aucune offre d'emploi disponible.</li>
-
+          <li>Aucune offre d'emploi disponible.</li>
         )}
-</ul>
-</div>
-
+      </ul>
+    </div>
   );
 
 };
--- frontend/src/components/MesAnnoncesPage/MesAnnoncesPage.jsx
+++ frontend/src/components/MesAnnoncesPage/MesAnnoncesPage.jsx
@@ -6,12 +6,16 @@ import NavBarEnt from '../NavBarEnt/NavBarEnt';
 const MesAnnoncesPage = () => {
   const location = useLocation();
   const connectedEmployerEmail = location.state?.connectedEmployerEmail; // Récupérer l'email de l'employeur
+
+  // Ajoute un console.log pour déboguer
+  console.log("Email de l'employeur connecté : ", connectedEmployerEmail);
+
   return (
-<div>
-<NavBarEnt/>
-<h1>Mes Annonces</h1>
-<MesAnnonces connectedEmployerEmail={connectedEmployerEmail} />
-</div>
+    <div>
+      <NavBarEnt />
+      <h1>Mes Annonces</h1>
+      <MesAnnonces connectedEmployerEmail={connectedEmployerEmail} />
+    </div>
   );
 };
 export default MesAnnoncesPage;
